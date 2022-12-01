4b4e37f5c9d6b08951501effeda6f8dfbe381371
Niklas <n.nikisch@gmail.com>
2022-12-01 13:23:44 +0100

    add jt for delete user

--- test/delete_user_test.go
+++ test/delete_user_test.go
@@ -3,8 +3,10 @@ package test
 import (
 	"net/http"
 	"testing"
+	"time"
 
 	"github.com/BeanCodeDe/authi/test/util"
+	"github.com/google/uuid"
 	"gopkg.in/go-playground/assert.v1"
 )
 
@@ -21,5 +23,33 @@ func TestDeleteUser_TokenNotValid(t *testing.T) {
 	status := util.DeleteUser(userId, token.AccessToken)
 	assert.Equal(t, status, http.StatusNoContent)
 	_, status = util.RefreshToken(userId, token.AccessToken, token.RefreshToken)
+	assert.Equal(t, status, http.StatusUnauthorized)
+}
+
+func TestDeleteUser_WrongFormatAccessToken(t *testing.T) {
+	userId := util.CreateUserForFurtherTesting(t)
+	status := util.DeleteUser(userId, "someWrongToken")
+	assert.Equal(t, status, http.StatusUnauthorized)
+}
+
+func TestDeleteUser_WrongUserIdToken(t *testing.T) {
+	userId := util.CreateUserForFurtherTesting(t)
+	signKey := util.LoadPrivatKeyFile(util.PrivatKeyFile)
+	customToken := util.CreateCustomJWTToken(uuid.NewString(), time.Now().Add(30*time.Minute).Unix(), signKey)
+	status := util.DeleteUser(userId, customToken)
+	assert.Equal(t, status, http.StatusUnauthorized)
+}
+
+func TestDeleteUser_WrongUserIdPath(t *testing.T) {
+	token, _ := util.ObtainToken(t)
+	status := util.DeleteUser(uuid.NewString(), token.AccessToken)
+	assert.Equal(t, status, http.StatusUnauthorized)
+}
+
+func TestDeleteUser_ExpiredToken(t *testing.T) {
+	userId := util.CreateUserForFurtherTesting(t)
+	signKey := util.LoadPrivatKeyFile(util.PrivatKeyFile)
+	customToken := util.CreateCustomJWTToken(userId, time.Now().Add(-1*time.Second).Unix(), signKey)
+	status := util.DeleteUser(userId, customToken)
 	assert.Equal(t, status, http.StatusUnauthorized)
 }
