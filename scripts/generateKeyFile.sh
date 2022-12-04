b0e9bebaba1d5634615aa6341674a3762fdebf7d
Niklas <n.nikisch@gmail.com>
2022-12-04 21:24:14 +0100

    generate public key in different way

--- scripts/generateKeyFile.sh
+++ scripts/generateKeyFile.sh
@@ -1,5 +1,5 @@
 mkdir ./deployments/token
 mkdir ./deployments/token/privat
 mkdir ./deployments/token/public
-ssh-keygen -t rsa -P "" -b 4096 -m PEM -f ./deployments/token/privat/jwtRS256.key
-ssh-keygen -e -m PEM -f ./deployments/token/privat/jwtRS256.key > ./deployments/token/public/jwtRS256.key.pub
+ssh-keygen -t rsa -b 4096 -m PEM -f ./deployments/token/privat/jwtRS256.key
+openssl rsa -in ./deployments/token/privat/jwtRS256.key -pubout -outform PEM -out ./deployments/token/public/jwtRS256.key.pub
