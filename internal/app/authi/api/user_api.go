e112ffd1e1c57c7704f8f1db09dc63b7c648002c
Niklas <n.nikisch@gmail.com>
2022-12-26 19:51:55 +0100

    check uuid

--- internal/app/authi/api/user_api.go
+++ internal/app/authi/api/user_api.go
@@ -214,7 +214,11 @@ func (userApi *UserApi) DeleteUser(context echo.Context) error {
 func setLoggerMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		correlationId := c.Request().Header.Get(correlationIdHeader)
-
+		_, err := uuid.Parse(correlationId)
+		if err != nil {
+			log.Warn("Correlation id is not from format uuid")
+			return echo.ErrBadRequest
+		}
 		logger := log.WithFields(log.Fields{
 			correlationIdHeader: correlationId,
 		})
