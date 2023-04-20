c03933cca26f9801976a61df102f866ca71bebfe
Bean900 <niklas_nikisch@MDXN01006660.bku.db.de>
2023-04-20 17:44:23 +0200

    #major refactor log of service

--- cmd/authi/authi.go
+++ cmd/authi/authi.go
@@ -1,16 +1,32 @@
 package main
 
 import (
+	"strings"
+
 	"github.com/BeanCodeDe/authi/internal/app/authi/api"
 	"github.com/BeanCodeDe/authi/internal/app/authi/util"
 	"github.com/BeanCodeDe/authi/pkg/parser"
 	log "github.com/sirupsen/logrus"
 )
 
+const banner = `
+ ______           __    __             
+/\  _  \         /\ \__/\ \      __    
+\ \ \L\ \  __  __\ \ ,_\ \ \___ /\_\   
+ \ \  __ \/\ \/\ \\ \ \/\ \  _  \/\ \  
+  \ \ \/\ \ \ \_\ \\ \ \_\ \ \ \ \ \ \ 
+   \ \_\ \_\ \____/ \ \__\\ \_\ \_\ \_\
+	\/_/\/_/\/___/   \/__/ \/_/\/_/\/_/
+____________________________________O/_______
+                                    O\
+`
+
 func main() {
 	setLogLevel()
-	log.Info("Start Server")
-
+	if log.GetLevel() > log.WarnLevel {
+		println(banner)
+	}
+	log.Debug("Start Server")
 	tokenParser, err := parser.NewJWTParser()
 	if err != nil {
 		log.Fatalf("Error while initializing token parser: %v", err)
@@ -25,12 +41,12 @@ func main() {
 
 func setLogLevel() {
 	logLevel := util.GetEnvWithFallback("LOG_LEVEL", "info")
-	switch logLevel {
+	switch strings.ToLower(logLevel) {
 	case "debug":
 		log.SetLevel(log.DebugLevel)
 	case "info":
 		log.SetLevel(log.InfoLevel)
-	case "warn":
+	case "warn", "warning":
 		log.SetLevel(log.WarnLevel)
 	}
 }
